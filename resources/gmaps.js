7d98ad9f8f7d4d48e1b3f6e760d079c80d051d72
emp1211 <erikpaffett@gmail.com>
2023-02-10 16:22:18 -0500

    Revert "Updated maps location"
    This reverts commit 3930be303af3aebe443e793aea34b77ed8726203.

--- resources/gmaps.js
+++ resources/gmaps.js
@@ -1,15 +1,15 @@
 // Initialize and add the map
 function initMap() {
-    // The location of fictional Arlen, TX
-    const arlen = { lat: 29.841720774726667, lng: -95.2557901 }; 
-    // The map, centered at Arlen
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.031 };
+    // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
-      center: arlen,
+      center: uluru,
     });
-    // The marker, positioned at Arlen
+    // The marker, positioned at Uluru
     const marker = new google.maps.Marker({
-      position: arlen,
+      position: uluru,
       map: map,
     });
   }
